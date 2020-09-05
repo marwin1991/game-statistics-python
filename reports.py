a19b2dd67e2538e02474af1058f47f52d7adb9d7
marwin1991 <peter.zmilczak@gmail.com>
2020-09-05 16:46:09 +0200

    Added gitignore and implemeted deicde fucntion

--- reports.py
+++ reports.py
@@ -7,8 +7,14 @@ def count_games(file_name): # (1) Count games
     return game_counter
 
 
-def decide(file_name, year): 
-    return "Not implemented yet :) "
+def decide(file_name, given_year): 
+    with open(file_name, "r") as file:
+        for line in file.readlines():
+            row_items = line.split("\t")
+            game_year = int(row_items[2])
+            if given_year == game_year:
+                return True
+        return False
 
 
 def get_latest(file_name):
@@ -29,7 +35,6 @@ def sort_abc(file_name):
 
 def get_genres(file_name):
     return "Not implemented yet :) "
-
 
 
 def when_was_top_sold_fps(file_name):
