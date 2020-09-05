d55497589a11e045bff0c36ca489476b41fbd263
marwin1991 <peter.zmilczak@gmail.com>
2020-09-05 14:44:01 +0200

    Created and implmented simple prining and system interaction

--- printing.py
+++ printing.py
@@ -1,1 +1,60 @@
 # Printing functions
+# from reports import *
+import reports
+import os
+
+COUNT_GAMES_NUMER = 1
+DECIDE_NUMBER = 2
+GET_LATEST_NUMBER = 3
+
+AVAILABE_FUNCTIONS_NUMBERS = [COUNT_GAMES_NUMER, DECIDE_NUMBER, GET_LATEST_NUMBER]
+
+def main():
+    file_with_data = "game_stat.txt"
+    print("Interactive printing module:")
+    function_number = get_function_to_start()
+    funtion_result = execute_function(function_number, file_with_data)
+    print(f"Function result: {funtion_result}")
+
+
+# This function decides which function to start
+# 1 - count_games
+# 2 - decide ...
+def get_function_to_start():
+    print_availble_functions()
+    user_choice = int(input("Which function you want to execute? : "))
+
+    if user_choice in AVAILABE_FUNCTIONS_NUMBERS:
+        return user_choice
+
+def execute_function(number, datafile):
+    if number == COUNT_GAMES_NUMER:
+        return reports.count_games(datafile)
+
+    if number == DECIDE_NUMBER:
+        return reports.decide(datafile, ask_for_a_year()) 
+
+    if number == GET_LATEST_NUMBER:
+        return reports.get_latest(datafile) 
+
+def ask_for_a_year():
+    string_year = input("Give a year :")
+    return int(string_year)
+
+def ask_for_genre():
+    genre = input("Give a genre :")
+    return genre
+
+def ask_for_argument(labal):
+    #os.clear("cls || clear")
+    return input(labal)
+
+
+def print_availble_functions():
+    print(f"({COUNT_GAMES_NUMER}) - Count games")
+    print(f"({DECIDE_NUMBER}) - Decide")
+    print(f"({GET_LATEST_NUMBER}) - Get latest")
+
+
+if __name__ == "__main__":
+    main()
--- reports.py
+++ reports.py
@@ -1,30 +1,36 @@
-def count_games(file_name):
-    pass
+def count_games(file_name): # (1) Count games
+    game_counter = 0
+    with open(file_name, "r") as file:
+        for line in file.readlines():
+            if len(line.replace("\n", "")) > 0:
+                game_counter += 1
+    return game_counter
 
 
-def decide(file_name, year):
-    pass
+def decide(file_name, year): 
+    return "Not implemented yet :) "
 
 
 def get_latest(file_name):
-    pass
+    return "Not implemented yet :) "
 
 
 def count_by_genre(file_name, genre):
-    pass
+    return "Not implemented yet :) "
 
 
 def get_line_number_by_title(file_name, title):
-    pass
+    return "Not implemented yet :) "
 
 
 def sort_abc(file_name):
-    pass
+    return "Not implemented yet :) "
 
 
 def get_genres(file_name):
-    pass
+    return "Not implemented yet :) "
+
 
 
 def when_was_top_sold_fps(file_name):
-    pass
+    return "Not implemented yet :) "
